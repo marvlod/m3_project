bbd6f0f524f32aa7d078990e291aedc729a803fa
vladimir <marvlod@gmail.com>
2023-03-13 23:33:33 +0300

    13_03

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='m3_project',
+    version='1.0.0',
+    packages=['m3_project', 'm3_project.app', 'm3_project.app.migrations', 'm3_project.m3_project'],
+    url='',
+    license='',
+    author='vladimir',
+    author_email='',
+    description='grade1 test'
+)
